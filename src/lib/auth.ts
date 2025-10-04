ad7a354190b91596ed86c0b94d6566b86321f470
mszulfiqar <mszulfiqarkhan@gmail.com>
2025-10-04 17:39:21 +0500

    dff

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,10 +22,10 @@ export const auth = betterAuth({
     },
     plugins: [nextCookies()],
     socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID! as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
-        },
+        // google: {
+        //     clientId: process.env.GOOGLE_CLIENT_ID! as string,
+        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
+        // },
         facebook: {
             clientId: process.env.FACEBOOK_CLIENT_ID! as string,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET! as string,
