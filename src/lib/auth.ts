99be4f4859f174fa2095f2ad08f712acbdb73289
mszulfiqar <mszulfiqarkhan@gmail.com>
2025-10-04 18:09:30 +0500

    dff

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,8 +27,8 @@ export const auth = betterAuth({
         //     clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
         // },
         facebook: {
-            clientId: process.env.FACEBOOK_CLIENT_ID! as string,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET! as string,
+            clientId: process.env.FACEBOOK_CLIENT_ID as string,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
         }
     }
 });
